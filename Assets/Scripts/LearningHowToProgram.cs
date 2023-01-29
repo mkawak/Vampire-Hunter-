11317323aa4ad145468898b789daf4108c13d543
Majd <majd@Majds-MacBook-Pro-16.local>
2023-01-29 15:54:40 -0800

    Initial Push

---
+++ Assets/Scripts/LearningHowToProgram.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningHowToProgram : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get Component<Rigi
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
