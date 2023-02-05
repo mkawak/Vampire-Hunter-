31d0ef62096b9164052b9ad97c9ec8a5e7357c3b
liz-areizaga <larei002@ucr.edu>
2023-02-05 13:59:18 -0800

    Enemy follows player--enemy needs mod to face player

---
+++ Vampire-Hunter-Enemy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = .05f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = player.position - transform.position; //tranform is the enemy since our script is attached to enemy character
+        //Atan2- calculates the angle between enemy and player
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //angle in degrees
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+        //Debug.Log(direction);
+    }
+    private void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+    void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    
+}
