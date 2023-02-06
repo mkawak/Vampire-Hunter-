6083c24fc9c4372c8ecd1180f7b718cabd650530
liz-areizaga <larei002@ucr.edu>
2023-02-06 01:55:53 -0800

    Flip enemy to walk in direction of player

--- Vampire-Hunter-Enemy/Assets/Scripts/Enemy.cs
+++ Vampire-Hunter-Enemy/Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     public float moveSpeed = 1f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool flip;
 
 
     private void Start()
@@ -18,17 +19,27 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         Vector3 direction = player.position - transform.position; //tranform is the enemy since our script is attached to enemy character
-        //Atan2- calculates the angle between enemy and player
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //angle in degrees
-        rb.rotation = angle;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; ////Atan2- calculates the angle between enemy and player
+        //rb.rotation = angle;
         direction.Normalize();
         movement = direction;
-        //Debug.Log(direction);
+        if (player.position.x > transform.position.x)
+        {
+            //player on right
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            //player on left
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
     }
+
     private void FixedUpdate()
     {
         MoveCharacter(movement);
     }
+
     void MoveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
