4afcc96b1dc6af141a56d69e3bccee03487c1916
Noor-A1 <nalag002@ucr.edu>
2023-03-13 07:17:30 -0700

    GameMaster compilation errors fix
    - updated API calls to camera
    - fixed call on player.position

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -6,21 +6,11 @@ public class GameMaster : MonoBehaviour
 {
     public static GameMaster instance;
 
-    //[SerializeField] public Transform player;
     [SerializeField] public int maxEnemies = 100;
     [SerializeField] public int secondsBetweenWaves = 10;
     [SerializeField] public GameObject enemy1; // enemy to spawn
     [SerializeField] public int enemiesPerWave = 5; 
     [SerializeField] public int targetFramesPerSecond = 60;
-
-    /* below vars are used to know the spawn limits on the current map, 
-    // ignored since spawning at placeholder location
-    [SerializeField] private float maxY = 590.0f;
-    [SerializeField] private float minY = 657.0f;
-    [SerializeField] private float maxX = 1072.0f;
-    [SerializeField] private float minX = 427.0f;
-    */
-
 
     private int framesPassed = 0;
     private GameObject player;
@@ -57,42 +47,42 @@ public class GameMaster : MonoBehaviour
 
                     // placeholder: spawn at center of map (x: 716, y: 401)                    
                     Vector3 spawnPosition = new Vector3(716.0f, 401.0f, 1f);
-                    if(player.position.x < 435)
+                    if(player.transform.position.x < 435)
                     {
                         min_xRange = 0.5f;
                     }
-                    if (player.position.y < 160)
+                    if (player.transform.position.y < 160)
                     {
                         min_yRange = 0.5f;
                     }
-                    if(player.position.x > 1060)
+                    if(player.transform.position.x > 1060)
                     {
                         max_xRange = 0.5f;
                     }
-                    if(player.position.y > 590)
+                    if(player.transform.position.y > 590)
                     {
                         max_yRange = 0.5f;
                     }
-                    if (randomSide == 0 && player.position.x >= 435)
+                    if (randomSide == 0 && player.transform.position.x >= 435)
                     {
-                        Vector3 leftPosition = camera.ViewportToWorldPoint(new Vector3(min_xRange, Random.Range(min_yRange, max_yRange), 1f));
+                        Vector3 leftPosition = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(min_xRange, Random.Range(min_yRange, max_yRange), 1f));
                         //Vector3 leftPosition = camera.ViewportToWorldPoint(new Vector3(0f, 0.5f, 1f));
                         spawnPosition = leftPosition;
                     }
-                    else if (randomSide == 1 && player.position.x <= 1060)
+                    else if (randomSide == 1 && player.transform.position.x <= 1060)
                     {
-                        Vector3 rightPosition = camera.ViewportToWorldPoint(new Vector3(max_xRange, Random.Range(min_yRange, max_yRange), 1f));
+                        Vector3 rightPosition = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(max_xRange, Random.Range(min_yRange, max_yRange), 1f));
                         spawnPosition = rightPosition;
                     }
-                    else if (randomSide == 2 && player.position.y <= 590)
+                    else if (randomSide == 2 && player.transform.position.y <= 590)
                     {
-                        Vector3 topPosition = camera.ViewportToWorldPoint(new Vector3(Random.Range(min_xRange, max_xRange), max_yRange, 1f));
+                        Vector3 topPosition = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(Random.Range(min_xRange, max_xRange), max_yRange, 1f));
                         //Vector3 topPosition = camera.ViewportToWorldPoint(new Vector3(0.5f, 1.0f, 1f));
                         spawnPosition = topPosition;
                     }
-                    else if (randomSide == 3 && player.position.y >= 160)
+                    else if (randomSide == 3 && player.transform.position.y >= 160)
                     {
-                        Vector3 bottomPosition = camera.ViewportToWorldPoint(new Vector3(Random.Range(min_xRange, max_xRange), min_yRange, 1f));
+                        Vector3 bottomPosition = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(Random.Range(min_xRange, max_xRange), min_yRange, 1f));
                         spawnPosition = bottomPosition;
                     }
 
